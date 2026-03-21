a89910d243f42b06de307da34fbcec119815b893
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-03-21 11:39:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,11 +17,16 @@ phone = os.environ.get("DJANGO_SUPERUSER_PHONE")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 if phone and password:
-    if not User.objects.filter(phone_number=phone).exists():
-        User.objects.create_superuser(phone_number=phone, password=password)
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
+    user, created = User.objects.get_or_create(phone_number=phone)
+
+    user.set_password(password)  # IMPORTANT (hash password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.first_name = "Admin"
+    user.last_name = "User"
+    user.save()
+
+    print("Superuser fixed/updated")
 EOF
 
 python manage.py collectstatic --no-input
